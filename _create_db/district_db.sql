3f7abb7250be50746500b7d77b0d7db538fb19e9
Phi Thuong <chontennguoidungcuaban166@gmail.com>
2021-11-29 07:57:09 +0700

    _create_db/

---
+++ _create_db/district_db.sql
@@ -0,0 +1,20 @@
+INSERT INTO
+    district (proID, disName) -- An Giang
+VALUES
+    (1, "Thành phố Long Xuyên"),
+    (1, "Thành phố Châu Đốc"),
+    (1, "Thị xã Tân Châu");
+
+INSERT INTO
+    district (proID, disName) -- Bà Rịa – Vũng Tàu
+VALUES
+    (2, "Thành phố Bà Rịa"),
+    (2, "Thành phố Vũng Tàu"),
+    (2, "Thị xã Phú Mỹ");
+
+INSERT INTO
+    district (proID, disName) -- Bắc Giang
+VALUES
+    (3, "Lục Ngạn"),
+    (3, "Sơn Động"),
+    (3, "Tân Yên");
---
+++ _create_db/town_db.sql
@@ -0,0 +1,62 @@
+INSERT INTO
+    town (proID, disID, townName) -- An Giang / Long Xuyen
+VALUES
+    (1, 1, "Bình Đức"),
+    (1, 1, "Bình Khánh"),
+    (1, 1, "Đông Xuyên");
+
+INSERT INTO
+    town (proID, disID, townName) -- An Giang / Chau Doc
+VALUES
+    (1, 2, "Châu Phú A"),
+    (1, 2, "Châu Phú B"),
+    (1, 2, "Núi Sam");
+
+INSERT INTO
+    town (proID, disID, townName) -- An Giang / Tan Chau
+VALUES
+    (1, 3, "Long Châu"),
+    (1, 3, "Long Hưng"),
+    (1, 3, "Long Phú");
+
+INSERT INTO
+    town (proID, disID, townName) -- Bà Rịa – Vũng Tàu / Bà Rịa
+VALUES
+    (2, 4, "Kim Dinh"),
+    (2, 4, "Long Hương"),
+    (2, 4, "Long Tâm");
+
+INSERT INTO
+    town (proID, disID, townName) -- Bà Rịa – Vũng Tàu / Vũng Tàu
+VALUES
+    (2, 5, "Thắng Nhất"),
+    (2, 5, "Thắng Nhi"),
+    (2, 5, "Thắng Tam");
+
+INSERT INTO
+    town (proID, disID, townName) -- Bà Rịa – Vũng Tàu / Phú Mỹ
+VALUES
+    (2, 6, "Châu Pha"),
+    (2, 6, "Mỹ Xuân"),
+    (2, 6, "Phước Hoà");
+
+INSERT INTO
+    town (proID, disID, townName) -- Bắc Giang / Lục Ngạn
+VALUES
+    (3, 7, "Đồng Cốc"),
+    (3, 7, "Giáp Sơn"),
+    (3, 7, "Hộ Đáp");
+
+INSERT INTO
+    town (proID, disID, townName) -- Bắc Giang / Sơn Động
+VALUES
+    (3, 8, "Tây Yên Tử"),
+    (3, 8, "Đại Sơn"),
+    (3, 8, "Phúc Sơn");
+
+INSERT INTO
+    town (proID, disID, townName) -- Bắc Giang / Tân Yên
+VALUES
+    (3, 9, "Ngọc Thiện"),
+    (3, 9, "Ngọc Vân"),
+    (3, 9, "Ngọc Lý");
